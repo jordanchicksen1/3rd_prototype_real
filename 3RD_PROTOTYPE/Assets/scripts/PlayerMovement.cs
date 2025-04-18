848aafb5fb6a460f174b3a88026ab5f15f5ea3bc
jordanchicksen1 <2333640@students.wits.ac.za>
2025-04-18 14:27:38 +0200

    did most of movement

---
+++ 3RD_PROTOTYPE/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+    public float groundDrag;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        if(grounded == true)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;    
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        //calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+}
