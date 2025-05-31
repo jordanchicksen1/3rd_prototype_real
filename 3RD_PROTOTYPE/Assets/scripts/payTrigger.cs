31d1cc105824dc6eaa14fb1e490ae80619e1bb94
jordanchicksen1 <2333640@students.wits.ac.za>
2025-05-31 23:36:32 +0200

    started coin gate

---
+++ 3RD_PROTOTYPE/Assets/scripts/payTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class payTrigger : MonoBehaviour
+{
+    public bool paid = false;
+    public bool isInPayingTrigger = false;
+
+    public GameObject gate;
+    public GameObject payingInstructions;
+    public GameObject notEnoughMoney;
+} 
