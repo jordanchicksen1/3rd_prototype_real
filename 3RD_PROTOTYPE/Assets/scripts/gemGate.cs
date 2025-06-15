4228fb19e6855e84651acb84789aa4ddbf2ff54a
jordanchicksen1 <2333640@students.wits.ac.za>
2025-06-15 12:30:52 +0200

    almost done with some stuff

--- 3RD_PROTOTYPE/Assets/scripts/gemGate.cs
+++ 3RD_PROTOTYPE/Assets/scripts/gemGate.cs
@@ -9,34 +9,44 @@ public class gemGate : MonoBehaviour
     public GameObject theGemGate;
     public gemManager gemManager;
 
+    public GameObject payPanel;
     public GameObject needMoreGems;
 
     public ParticleSystem sparkle;
 
     public AudioSource SFX;
     public AudioClip openedGate;
+    public AudioClip denied;
+
+    public bool hasPressedButton = false;
 
     public void PayGem()
     {
-        if(gemManager.gem > 2.99)
+        if (gemManager.gem > 2.99 && hasPressedButton == false)
         {
             StartCoroutine(OpenGate());
+            gemManager.payGem();
+            hasPressedButton = true;
         }
 
-        if(gemManager.gem < 2.99)
+        if(gemManager.gem < 2.99 && hasPressedButton == false)
         {
             StartCoroutine(NotEnoughGems());
+            hasPressedButton = true;
         }
     }
 
     public IEnumerator OpenGate()
     {
+        
         yield return new WaitForSeconds(0f);
-        gemManager.payGem();
         sparkle.Play();
         SFX.clip = openedGate;
         SFX.Play();
-        gemManager.payGem();
+        payPanel.SetActive(false);
+        hasPressedButton = false ;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         yield return new WaitForSeconds(0.1f);
         Destroy(theGemGate);
     }
@@ -45,6 +55,9 @@ public class gemGate : MonoBehaviour
     {
         yield return new WaitForSeconds(0f);   
         needMoreGems.SetActive(true);
+        SFX.clip = denied;
+        SFX.Play();
+        hasPressedButton = false;
         yield return new WaitForSeconds(2f);
         needMoreGems.SetActive(false);
     }
